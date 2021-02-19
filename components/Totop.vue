51c6bfaa0326b6da67b5fbbe9a0c1e56d469d05c
叶聪 <YQCyecong@163.com>
2021-02-19 18:10:15 +0800

    增加up组件

---
+++ components/Totop.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- up-top -->
+  <a-back-top>
+    <div class="ant-back-top-inner">UP</div>
+  </a-back-top>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+// up-top
+.ant-back-top-inner {
+  cursor: @cursor-pointer;
+  transition: all 0.5s ease;
+  height: 100%;
+  line-height: 36px;
+  text-align: center;
+  border: 2px dotted @border-color-base;
+  border-radius: 20px;
+  box-shadow: @box-shadow-base;
+  &:hover {
+    box-shadow: 0 2px 10px rgba(255, 0, 0, 0.5);
+  }
+}
+</style>
